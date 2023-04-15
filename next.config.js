e53dfdb6ad36dccf7d037e7d0465cafbfd770eb8
Igor Dudek <igor.dudek@firma.interia.pl>
2023-04-15 12:06:05 +0200

    refactor: for static build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export", // for build static site
+  // distDir: "dist", // change the output director
+  images: {
+    unoptimized: true, // disable the Image Optimization API
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
