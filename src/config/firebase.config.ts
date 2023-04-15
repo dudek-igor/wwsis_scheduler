c709965dccff38c11427702edaee5ec96b581ad3
Igor Dudek <igor.dudek@firma.interia.pl>
2023-04-15 13:18:52 +0200

    feat: add firebase to project

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,9 @@
+// Web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "scheduler-98e2e.firebaseapp.com",
+  projectId: "scheduler-98e2e",
+  storageBucket: "scheduler-98e2e.appspot.com",
+  messagingSenderId: "796658478688",
+  appId: "1:796658478688:web:a3d1b7cdfb00ff4d1f5d05",
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { app } from "@/firebase";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // console.log(app);
+  return <Component {...pageProps} />;
 }
