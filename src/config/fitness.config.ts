b121b51fedc6deab69f645c4b537b483c9b68c5b
Igor Dudek <igor.dudek@firma.interia.pl>
2023-04-19 23:04:18 +0200

    refactor: move difficultyColors to config

---
+++ src/config/fitness.config.ts
@@ -0,0 +1,3 @@
+import { red, blue, green, grey } from "@mui/material/colors";
+
+export const difficultyColors = [grey[500], green[500], blue[500], red[500]];
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export * from "./firebase.config";
 export * from "./particles.config";
 export * from "./days.config";
+export * from "./fitness.config";
